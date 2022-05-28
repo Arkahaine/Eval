0cf28a69570dae0a07f4f771c0f3c6af06350085
unknown <you@example.com>
2022-05-28 21:10:24 +0200

    Exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'statistique';
+  statistique1: Statistique = new Statistique("1", "France", "68 125 478");
+  statistique2: Statistique = new Statistique("2", "Italie", "54 012 354");
 }

ffa996e903e7a7795b123d0b256c7aa683252e65
Arkahaine <52594852+Arkahaine@users.noreply.github.com>
2022-05-29 21:43:25 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Statistique } from './models/statistique';
 })
 export class AppComponent {
   statistiques: Statistique[] = [
-    new Statistique("1", "Chine", "1 448 471 000"),
-    new Statistique("2", "Inde", "1 406 632 000")];
+    new Statistique("1", "France", "68 125 478"),
+    new Statistique("2", "Italie", "54 012 354")];
 
     constructor(){
       setTimeout(() => {
-        this.statistiques.push(new Statistique("2", "Etats-Unis", "334 805 000"));
+        this.statistiques.push(new Statistique("3", "Suisse", "24 145 015"));
       }, 3000);
     }
 }
